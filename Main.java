2df117fd62bc41862c07558ecaf4394cc503f096
Trevor Tomesh <slugzzz@gmail.com>
2026-01-31 14:24:06 -0600

    Add README and initial Main.java implementation

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+
+        // Display message Hello World! on the console
+        System.out.println("Hello World!");
+        System.out.println("Hello again!");
+        System.out.println("This is silly.");
+
+        System.out.print("(10.5+2*3)/(45-3.5) = ");
+        System.out.println((10.5+2*3)/(45-3.5));
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# CIDS162-SP26
+# CIDS 162 – Computer Programming
+
+This repository is for CIDS 162 – Computer Programming. Updates to the daily coding exercises and examples will be posted here throughout the term.
+
+Check back regularly for new materials and commits.
