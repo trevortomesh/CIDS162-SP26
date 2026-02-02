ec1f92857ea2a3b244eef066073f5d09f1026870
Trevor Tomesh <slugzzz@gmail.com>
2026-02-02 10:51:08 -0600

    Refactor Main.java to comment out example outputs and calculations

--- Main.java
+++ Main.java
@@ -2,13 +2,24 @@ public class Main {
     public static void main(String[] args){
 
         // Display message Hello World! on the console
-        System.out.println("Hello World!");
-        System.out.println("Hello again!");
-        System.out.println("This is silly.");
+        // System.out.println("Hello World!");
+        // System.out.println("Hello again!");
+        // System.out.println("This is silly.");
 
-        System.out.print("(10.5+2*3)/(45-3.5) = ");
-        System.out.println((10.5+2*3)/(45-3.5));
+        // System.out.print("(10.5+2*3)/(45-3.5) = ");
+        // System.out.println((10.5+2*3)/(45-3.5));
 
+        // Celsius to Fahrenheit Calculator
+        double c;
+        c = 35.2;
+        System.out.print("Celsius " + c + " is Fahrenheit: ");
+        System.out.println((9.0/5) * c + 32);
+        //System.out.println(9/5);
+
+
+        // Data types in java!
+        byte b = 100; // 8 bits -> -128 to 128
+        System.out.println("b is " + b);
 
     }
 }
