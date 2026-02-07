364a980f2308f433d77c39cb7f1851512d1287ad
Trevor Tomesh <slugzzz@gmail.com>
2026-02-06 20:59:18 -0600

    Refactor Main.java to comment out additional example outputs and calculations

--- Main.java
+++ Main.java
@@ -10,16 +10,70 @@ public class Main {
         // System.out.println((10.5+2*3)/(45-3.5));
 
         // Celsius to Fahrenheit Calculator
-        double c;
-        c = 35.2;
-        System.out.print("Celsius " + c + " is Fahrenheit: ");
-        System.out.println((9.0/5) * c + 32);
-        //System.out.println(9/5);
+    //     double c;
+    //     c = 35.2;
+    //     System.out.print("Celsius " + c + " is Fahrenheit: ");
+    //     System.out.println((9.0/5) * c + 32);
+    //     //System.out.println(9/5);
+
+    //     // python
+    //     /*
+    //          x = 5
+    //          x = "hello"
+    //          x = 3.14159
+    //     */
+
+    // //    int x = 5;
+    // //    x = 5.0; //compile time error!
+
+    //     // Data types in java!
+    //     // *** TYPES OF INTEGERS *** //
+    //     byte b = 10; // 8 bits -> -128 to 128
+    //     short s = 10000; // 2 bytes
+    //     int myInt = 400000; // 4 bytes
+    //     long myLong = 922337203; // 8 bytes;
+
+    //     //*** DECIMAL TYPES *****/
+    //     float myFloat = 223312.3451f;
+    //     double myDouble = 12.3;
+
+    //     //*********** OTHERS ********/
+    //     char myChar = 'a';
+    //     char myChar2 = '@';
+
+    //     char ch; 
+    //     ch = 'A';
+    //     int code = ch;
+    //     System.out.println(code);
+
+    //     boolean isTrue = true;
+    //     isTrue = false;
 
 
-        // Data types in java!
-        byte b = 180; // 8 bits -> -128 to 128
-        System.out.println("b is " + b);
+    //     System.out.println("b is " + b);
+
+    //     int a = 12;
+    //     char aChar = 'a';
+    //     String x = "Hello world!"; // derived data type 
+    //     // a = "hello world!";
+    //     // a = 25.2;
+
+    char a = 'H';
+    char b = 'e';
+    char c = 'l';
+    char d = 'l';
+    char f = 'o';
+    char g = '!';
+    String hello = "Hello!";
+    System.out.println(hello);
+
+    // nums = [1,2,3]
+    int[] nums = {1,2,3};
+    System.out.println(nums[0]);
+
+    double db = 3.2;
+    int x = db;
+
 
     }
 }
