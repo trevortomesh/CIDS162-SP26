13138028447528cb835348bfd0cb044b1a9278f2
Trevor Tomesh <slugzzz@gmail.com>
2026-03-04 11:42:59 -0600

    Refactor Main.java to implement a time display loop and clean up commented-out code

--- Main.java
+++ Main.java
@@ -1,27 +1,58 @@
 import java.util.Scanner;
 
-public class Main_old {
+public class Main {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
+    //   Scanner input = new Scanner(System.in);
 
-
-        int number = (int)(Math.random() * 101);
-
-        System.out.println("Guess a magic number between 0 and 100");
-        int guess = -1;
-        while(guess != number){
-            System.out.print("\nEnter your guess: ");
-            guess = input.nextInt();
-        
-            if(number == guess){
-                System.out.println("You win... or something");
-                //break;
-            }else if(number < guess){
-                System.out.println("Too high, bro...");
-            }else{System.out.println("Too low... :-(");}
+    for(int min = 0; min < 60; min++){
+        for(int sec = 0; sec < 60; sec++){
+            System.out.println(min + " minutes and " + sec + " seconds");
         }
+    }   
 
-        input.close();
+    // int i = 0;
+    // while(i < 100){
+    //     System.out.println(i);
+    //     i++;
+    // }
+
+    // int sum = 0;
+    // int data = 0;
+
+    //     do{
+    //         System.out.print("Enter an integer (the input ends if it is 0): ");
+    //         data = input.nextInt();
+    //         sum += data;
+    //     }while(data != 0);
+  
+
+    //     System.out.println("The sum is " + sum);
+
+        // char continueLoop = 'Y';
+
+        // while(continueLoop == 'Y'){
+        //     System.out.println("Yay!");
+        //     System.out.print("Enter Y to continue and N to quit: ");
+        //     continueLoop = input.nextLine().charAt(0);
+        // }
+
+        // // int number = (int)(Math.random() * 101);
+
+        // System.out.println("Guess a magic number between 0 and 100");
+        // int guess = -1;
+        // while(guess != number){
+        //     System.out.print("\nEnter your guess: ");
+        //     guess = input.nextInt();
+        
+        //     if(number == guess){
+        //         System.out.println("You win... or something");
+        //         //break;
+        //     }else if(number < guess){
+        //         System.out.println("Too high, bro...");
+        //     }else{System.out.println("Too low... :-(");}
+        // }
+
+        //input.close();
         // int sum = 0; 
         // int i = 1;
         // while(i < 10){
