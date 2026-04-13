f12b5fc1425d22e3f61e036def2d8aa43b344dc4
Trevor Tomesh <slugzzz@gmail.com>
2026-04-13 10:49:16 -0500

    Refactor Main.java to add array manipulation methods and clean up commented-out code

--- Main.java
+++ Main.java
@@ -4,21 +4,70 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
-        int[] numbers = new int[4];
-        //numbers = {3,2,9,0};
-        numbers[0] = 3;
-        numbers[1] = 2;
-        numbers[2] = 9;
-        numbers[3] = 0;
 
-        int[] newNumbers = {1,2,3,4};
-        int[] newerNumbers = newNumbers;
+        int[] testArray = {1,4,4,2,5,-3,6,2};
+        int[] orderedArray = {1, 10, 16, 25, 29, 33, 34, 62, 100, 120, 121};
+        //int j = linearSearch(testArray, -3);
+        int k = binarySearch(orderedArray, 120);
+        int j = linearSearch(orderedArray, 120);
+        System.out.println(k);
+        System.out.println(j);
 
-        newNumbers[2] = 10000;
-        System.out.println(newNumbers[2]);
-        System.out.println(newerNumbers[2]);
-        System.out.println("newNumbers address: " + newNumbers);
-        System.out.println("newerNumbers address: " + newerNumbers);
+
+
+
+        // int[] pokenum = {1, 10, 16, 25, 29};
+        // int target = 10;
+        // boolean is_found = false; 
+
+        // for(int i = 0; i < pokenum.length; i++){
+        //     if(pokenum[i] == target){
+        //         is_found = true;
+        //         break;
+        //     }
+        // }
+        // if(!is_found){
+        //         System.out.println("Pokemon not found :-( ");
+        // }else{
+        //         System.out.println("Pokemon found!");
+        // }
+            
+        
+        //int[] mainArray = returnArray(10);
+        //mainArray = reverse(mainArray);
+        //int[] reverseArray = reverse(mainArray);
+       // printArray(mainArray);
+        //printArray(reverseArray);
+
+        // int[] numbers = new int[4];
+        // System.out.println(numbers[0]);
+        // //numbers = {3,2,9,0};
+        // numbers[0] = 3;
+        // numbers[1] = 2;
+        // numbers[2] = 9;
+        // numbers[3] = 0;
+
+        // printArray(numbers);
+
+        // int[] newNumbers = {1,2,3,4};
+        // int x = 10;
+
+        // setIntZero(x);
+        // System.out.println("x is now " + x);
+        // setArrayZero(newNumbers);
+        
+        // for(int i = 0; i < newNumbers.length; i++){
+        //     System.out.print(newNumbers[i] + " ");
+        // }
+        // System.out.println();
+
+       // int[] newerNumbers = newNumbers;
+
+        // newNumbers[2] = 10000;
+        // System.out.println(newNumbers[2]);
+        // System.out.println(newerNumbers[2]);
+        // System.out.println("newNumbers address: " + newNumbers);
+        // System.out.println("newerNumbers address: " + newerNumbers);
 
         // System.out.println(newNumbers);
         // System.out.println(newerNumbers);
@@ -113,12 +162,87 @@ public class Main {
         //System.out.println(j);
     }
 
+    public static void printNum(int num){
+        System.out.println(num);
+    }
+
+    public static void printArray(int[] array){
+        for(int i = 0; i < array.length; i++){
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void setArrayZero(int[] array){
+        for(int i = 0; i < array.length; i++){
+            array[i] = 0;
+        }
+    }
+
+    public static void setIntZero(int x){
+        x = 0;
+        System.out.println(x);
+    }
+
     static void localVar(int i){
         int k = 1;
         i = 1000;
         System.out.println("i in localVar is " + i);
         System.out.println("k is " + k);
     }
+
+    static int[] returnArray(int x){
+        int[] myArray = new int[x];
+        for(int i = 0; i < myArray.length; i++){
+            myArray[i] = i;
+        }
+        return myArray;
+    }
+
+    static int[] reverse(int[] list){
+        int[] result = new int[list.length];
+        for(int i = 0, j = list.length-1; i < list.length; i++, j--){
+            result[j] = list[i];
+        }
+        return result;
+    }
+
+    public static int linearSearch(int[] list, int key){
+        int numSteps = 0;
+        for(int i = 0; i < list.length; i++){
+            numSteps++;
+            if(key == list[i]){
+                System.out.println("Number of steps: " + numSteps);
+                return i;
+            }
+        }
+        System.out.println("Number of steps: " + numSteps);
+        return -1;
+    }
+
+    public static int binarySearch(
+        int[] list, int key){
+
+            int low = 0;
+            int high = list.length -1;
+            int steps = 0;
+            while(high >= low){
+                steps++;
+                int mid = (low + high)/2;
+                if(key < list[mid]){
+                    high = mid - 1;
+                }else if (key == list[mid]){
+                    System.out.println("Number of steps: " + steps);
+                    return mid;
+                }else{
+                    low = mid+1;
+                }
+
+            }
+            System.out.println("Number of steps " + steps);
+            return -1;
+    
+        }
 
 }
 
