ad8d740397ab778f4e49005f5f75f644742d23f8
Trevor Tomesh <slugzzz@gmail.com>
2026-04-22 12:26:23 -0500

    Refactor Main.java to implement functionality for reading points and calculating the closest two points, while cleaning up commented-out code

--- Main.java
+++ Main.java
@@ -4,11 +4,45 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of points: ");
+        int numberOfPoints = input.nextInt();
 
+         double[][] points = new double[numberOfPoints][2];
+        System.out.print("Enter " + numberOfPoints + " points: ");
+        for(int i = 0; i < points.length; i++){
+            points[i][0] = input.nextDouble();
+            points[i][1] = input.nextDouble();
+        }
 
-        int[][] matrix = randomMatrix(5,5);
-        printMatrix(matrix);
-        System.out.println(sumMatrix(matrix));
+        int p1 = 0, p2 = 1;
+        double shortestDistance = distance(points[p1][0], points[p1][1],
+                                           points[p2][0], points[p2][1]);
+
+        for(int i = 0; i < points.length; i++){
+            for(int j = i+1; j < points.length; j++){
+                double distance = distance(points[i][0], points[i][1],
+                                           points[j][0], points[j][1]);
+
+                if(shortestDistance > distance){
+                    p1 = i;  //new point 1
+                    p2 = j;  //new point 2
+                    shortestDistance = distance;
+                }
+            }
+        }
+
+        System.out.println("The closest two points are " +
+            "("+points[p1][0] + ", " + points[p1][1] + ") and (" +
+            points[p2][0] + ", " + points[p2][1]+")"); 
+
+        // shortestDistance = distance(25, 3,
+        //                             1, 2);
+       // System.out.println(shortestDistance);
+
+        // int[][] matrix = randomMatrix(5,5);
+        // printMatrix(matrix);
+        // System.out.println(sumMatrix(matrix));
 
         // int[][] triangleArray ={
         //     {1,2,3,4,5},
@@ -339,6 +373,10 @@ public class Main {
             }
         }
         return total;
+    }
+
+    public static double distance(double x1, double y1, double x2, double y2){
+        return Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
     }
 
 }
